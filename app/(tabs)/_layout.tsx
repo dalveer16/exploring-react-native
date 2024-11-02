61ab0a7b8fd1212d8077d86a9280f39fb0965124
Dalveer Singh <57098009+dalveer16@users.noreply.github.com>
2024-11-02 22:39:22 +0530

    initail setup

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,10 @@
+import { Slot } from "expo-router";
+import React from "react";
+
+import { useColorScheme } from "@/hooks/useColorScheme";
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return <Slot />;
+}
